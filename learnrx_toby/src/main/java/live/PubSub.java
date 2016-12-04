84f262c0baf7f737ce65b27b416f1010a68add7b
arahansa <arahansa@naver.com>
2016-12-04 11:44:20 +0900

    first commit

---
+++ learnrx_toby/src/main/java/live/PubSub.java
@@ -0,0 +1,73 @@
+package live;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by jarvis on 2016. 12. 3..
+ */
+
+public class PubSub {
+  public static void main(String[] args) {
+    List<Integer> iter = Stream.iterate(1, a -> a + 1).limit(10).collect(toList());
+
+
+    // Publisher
+    Publisher<Integer> pub = new Publisher<Integer>() {
+      @Override
+      public void subscribe(org.reactivestreams.Subscriber<? super Integer> sub) {
+        sub.onSubscribe(new Subscription() {
+          @Override
+          public void request(long n) {
+            try{
+              iter.forEach(s->sub.onNext(s));
+              sub.onComplete();
+            }catch(Throwable t){
+              sub.onError(t);
+            }
+          }
+
+          @Override
+          public void cancel() {
+
+          }
+        });
+      }
+    };
+
+    Subscriber<Integer> sub = new Subscriber<Integer>() {
+      @Override
+      public void onSubscribe(Subscription s) {
+        System.out.println("onSub");
+        s.request(Long.MAX_VALUE);
+      }
+
+      @Override
+      public void onNext(Integer i) {
+        System.out.println("onNext:"+i);
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        System.out.println("onError");
+      }
+
+      @Override
+      public void onComplete() {
+        System.out.println("onComplete");
+      }
+    };
+
+    pub.subscribe(sub);
+
+  }
+
+
+}
---
+++ toby_tv_live/src/live/PubSub.java
@@ -0,0 +1,12 @@
+package live;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by jarvis on 2016. 12. 3..
+ */
+public class PubSub {
+  public static void main(String[] args) {
+    Publisher<Integer> =Stream.iterate(1, a->a+1)
+  }
+}
