d024d0a6d09fc00b14d819fa8ee06dfd4d9e4486
arahansa <arahansa@naver.com>
2016-12-18 23:03:24 +0900

    메인쓰레드가 종료가 되도 죽지가 않아 ? 인터벌 시험

--- springreact/src/main/java/live/FluxScEx.java
+++ springreact/src/main/java/live/FluxScEx.java
@@ -1,19 +1,20 @@
 package live;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jarvis on 2016. 12. 18..
  */
+@Slf4j
 public class FluxScEx {
-  public static void main(String[] args) {
-    Flux.range(1, 10)
-            .publishOn(Schedulers.newSingle("pub"))
-            .log()
-            .subscribeOn(Schedulers.newSingle("sub"))
-            .subscribe(System.out::println);
-
-    System.out.println("Exit.");
+  public static void main(String[] args) throws InterruptedException {
+    Flux.interval(Duration.ofMillis(500))
+        .subscribe(s->{log.debug("onNext:{}", s);});
+    log.debug("exit");
+    TimeUnit.SECONDS.sleep(5);
   }
 }
