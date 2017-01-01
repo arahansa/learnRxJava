1cc0d9b083ba7621a5a185a40c74103417863dd4
arahansa <arahansa@naver.com>
2017-01-01 17:46:26 +0900

    스프링 부트에 어싱크 적용

--- springreact/src/main/java/com/arahansa/SpringreactApplication.java
+++ springreact/src/main/java/com/arahansa/SpringreactApplication.java
@@ -1,14 +1,27 @@
 package com.arahansa;
 
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Future;
+
+@Slf4j
 @SpringBootApplication
+@EnableAsync
 public class SpringreactApplication {
 
 	@RestController
@@ -32,7 +45,33 @@ public class SpringreactApplication {
 		}
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringreactApplication.class, args);
+	@Component
+	public static class MyService{
+		@Async
+		public Future<String> hello() throws InterruptedException {
+			log.info("hello()");
+			Thread.sleep(2000);
+			return new AsyncResult<>("Hello");
+		}
 	}
+
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext c = SpringApplication.run(SpringreactApplication.class, args)){
+
+		}
+	}
+
+	@Autowired MyService myService;
+
+	@Bean
+	ApplicationRunner run(){
+		return args->{
+			log.info("run()");
+			final Future<String> hello = myService.hello();
+			log.info("exit : "+ hello.isDone());
+			log.info("exit "+ hello.get());
+		};
+	}
+
+
 }
--- springreact/src/main/java/toby/FutureEx.java
+++ springreact/src/main/java/toby/FutureEx.java
@@ -5,6 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Objects;
 import java.util.concurrent.*;
 
+// Future
+// Callback
+// .. ?
+
 /**
  * Created by arahansa on 2017-01-01.
  */
