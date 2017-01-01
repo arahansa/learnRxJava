08a5d06cba70d7c1c1d04447c73bf5b98a05d9e0
arahansa <arahansa@naver.com>
2017-01-01 22:35:55 +0900

    비동기 관련 일반 스프링으로 옮김..

--- springreact/src/main/java/com/arahansa/SpringreactApplication.java
+++ springreact/src/main/java/com/arahansa/SpringreactApplication.java
@@ -1,93 +1,21 @@
 package com.arahansa;
 
 import lombok.extern.slf4j.Slf4j;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.expression.spel.InternalParseException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.async.DeferredResult;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
-
-import java.io.IOException;
-import java.util.Queue;
-import java.util.concurrent.*;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @Slf4j
 @SpringBootApplication
 @EnableAsync
 public class SpringreactApplication {
-
-	@RestController
-	public static class MyController {
-
-		@GetMapping("/emitter")
-		public ResponseBodyEmitter emitter(){
-			ResponseBodyEmitter emitter = new ResponseBodyEmitter();
-			Executors.newSingleThreadExecutor().submit(()->{
-				try {
-					for(int i=0;i<=50;i++){
-					emitter.send("<p>Stream " + i +"</p>");
-					Thread.sleep(2000);
-					}
-				} catch (Exception e) {}
-			});
-			return emitter;
-		}
-
-		Queue<DeferredResult<String>> results = new ConcurrentLinkedQueue<>();
-
-		@GetMapping("/dr")
-		public DeferredResult<String> dr() throws InterruptedException{
-			log.info("dr");
-			DeferredResult<String> dr = new DeferredResult<>(60000000L);
-			results.add(dr);
-			return dr;
-		}
-
-		@GetMapping("/dr/count")
-		public String drcount(){
-			return String.valueOf(results.size());
-		}
-
-		@GetMapping("/dr/event")
-		public String drcount(String msg){
-			for(DeferredResult<String> dr : results){
-				dr.setResult("Hello " + msg);
-				results.remove(dr);
-			}
-			return "OK";
-		}
-
-		@GetMapping("/callable")
-		public String callable() throws InterruptedException {
-			log.info("async");
-			Thread.sleep(2000);
-			return "hello";
-		}
-//		public Callable<String> callable() throws InterruptedException {
-//			log.info("callable");
-//			return () -> {
-//				log.info("async");
-//				Thread.sleep(1000);
-//				return "hello";
-//			};
-//		}
-	}
 
 	@Component
 	public static class MyService{
