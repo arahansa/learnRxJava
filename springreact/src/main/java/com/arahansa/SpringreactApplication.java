3ca6cdcb346a15118e02542e4017fcd2b9ebb718
arahansa <arahansa@naver.com>
2017-01-01 21:47:05 +0900

    add Emitter

--- springreact/src/main/java/com/arahansa/SpringreactApplication.java
+++ springreact/src/main/java/com/arahansa/SpringreactApplication.java
@@ -21,7 +21,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
+import java.io.IOException;
 import java.util.Queue;
 import java.util.concurrent.*;
 
@@ -32,6 +34,20 @@ public class SpringreactApplication {
 
 	@RestController
 	public static class MyController {
+
+		@GetMapping("/emitter")
+		public ResponseBodyEmitter emitter(){
+			ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+			Executors.newSingleThreadExecutor().submit(()->{
+				try {
+					for(int i=0;i<=50;i++){
+					emitter.send("<p>Stream " + i +"</p>");
+					Thread.sleep(2000);
+					}
+				} catch (Exception e) {}
+			});
+			return emitter;
+		}
 
 		Queue<DeferredResult<String>> results = new ConcurrentLinkedQueue<>();
 
@@ -72,8 +88,6 @@ public class SpringreactApplication {
 //			};
 //		}
 	}
-
-
 
 	@Component
 	public static class MyService{
