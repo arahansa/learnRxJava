cb0e84217a62f46abaacea274376041850055326
arahansa <arahansa@naver.com>
2017-01-01 15:33:38 +0900

    FutureTask 로 변경

--- springreact/src/main/java/toby/FutureEx.java
+++ springreact/src/main/java/toby/FutureEx.java
@@ -2,10 +2,7 @@ package toby;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 /**
  * Created by arahansa on 2017-01-01.
@@ -13,13 +10,20 @@ import java.util.concurrent.Future;
 @Slf4j
 public class FutureEx {
 
+    // Future
+    // Callback
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService es = Executors.newCachedThreadPool();
-        Future<String> f = es.submit(() -> {
+
+        FutureTask<String> f = new FutureTask<>(()->{
             Thread.sleep(2000);
             log.info("Async");
             return "Hello";
         });
+
+        es.execute(f);
+
         System.out.println(f.isDone());
         Thread.sleep(2100);
         log.info("Exit");
